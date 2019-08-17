6305ce90d0b8c87858f2f1db87575aa60d4c2bc4
张陈浩 <chenhao.zhang@2piday.com>
2019-08-17 13:17:25 +0800

    add code

---
+++ util-swing/src/main/java/com/zifang/util/swing/algrithm/lesson2/a/Client.java
@@ -0,0 +1,11 @@
+package com.zifang.util.swing.algrithm.lesson2.a;
+
+public class Client {
+    public static void main(String[] args) {
+        int sceneWidth = 800;
+        int sceneHeight = 800;
+        int N = 10;
+
+        AlgoVisualizer visualizer = new AlgoVisualizer(sceneWidth, sceneHeight, N);
+    }
+}
