9d5e083fb90fde4de728a054dfced556476beb14
张陈浩 <chenhao.zhang@2piday.com>
2019-08-20 16:13:38 +0800

    add code

--- util-core/src/main/java/com/zifang/util/core/annoations/AnnoationsUtils.java
+++ util-core/src/main/java/com/zifang/util/core/annoations/AnnoationsUtils.java
@@ -1,5 +1,12 @@
 package com.zifang.util.core.annoations;
 
+
+/**
+ * Annoation 相关的工具类
+ *
+ *
+ *
+ * */
 public class AnnoationsUtils {
 //    public static List<>
 }
---
+++ util-core/src/main/java/com/zifang/util/core/beans/converter/impl/ConvertChan.java
@@ -0,0 +1,8 @@
+package com.zifang.util.core.beans.converter.impl;
+
+
+
+////提供转换链条
+////提供全部转换的子类部分
+public class ConvertChan {
+}
