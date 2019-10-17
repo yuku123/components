a5f03e17facc5be5f64e286573884b471d916251
张陈浩 <chenhao.zhang@2piday.com>
2019-10-17 18:01:44 +0800

    add code

---
+++ util-core/src/main/java/com/zifang/util/core/demo/disrupt/LongEventFactory.java
@@ -0,0 +1,12 @@
+package com.zifang.util.core.demo.disrupt;
+
+import com.lmax.disruptor.EventFactory;
+
+//产生LongEvent的工厂类，它会在Disruptor系统初始化时，构造所有的缓冲区中的对象实例（预先分配空间）
+public class LongEventFactory implements EventFactory<LongEvent>
+{
+    public LongEvent newInstance()
+    {
+        return new LongEvent();
+    }
+}
