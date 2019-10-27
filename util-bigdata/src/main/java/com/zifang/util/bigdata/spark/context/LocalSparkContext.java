018170d6cdcf87609658a8a491f250267cceef3a
张陈浩 <chenhao.zhang@2piday.com>
2019-10-27 23:51:51 +0800

    add code

--- util-bigdata/src/main/java/com/zifang/util/bigdata/spark/context/LocalSparkContext.java
+++ util-bigdata/src/main/java/com/zifang/util/bigdata/spark/context/LocalSparkContext.java
@@ -4,6 +4,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SQLContext;
 import org.apache.spark.SparkContext;
+import org.apache.spark.sql.SparkSession;
 
 public class LocalSparkContext {
 
@@ -15,11 +16,24 @@ public class LocalSparkContext {
 
     private SparkContext sparkContext;
 
+    private SparkSession sparkSession;
+
     LocalSparkContext(){
         sparkConf = new SparkConf();
-        sparkConf.setMaster("local").setAppName("test");
-        javaSparkContext = new JavaSparkContext(sparkConf);
-        sqlContext = new SQLContext(javaSparkContext);
+        sparkConf.setMaster("local[4]").setAppName("test");
+//        javaSparkContext = new JavaSparkContext(sparkConf);
+//        sqlContext = new SQLContext(javaSparkContext);
+
+        sparkSession = SparkSession.builder()
+                .master("local[4]")
+                .appName("appName")
+                .config(sparkConf)
+                .getOrCreate();
+        sparkContext = sparkSession.sparkContext();
+        sparkContext.setLogLevel("ERROR");
+        javaSparkContext = JavaSparkContext.fromSparkContext(sparkContext);
+        sqlContext = sparkSession.sqlContext();
+
 //        sparkContext = new SparkContext(sparkConf);
 //        sparkContext.setLogLevel("ERROR");
     }
