b3f8e2fea4e54679330e2a238d8f0402e26588d9
张陈浩 <chenhao.zhang@2piday.com>
2019-11-24 16:30:17 +0800

    add code

---
+++ util-swing/src/main/java/com/zifang/util/swing/workflow/App.java
@@ -0,0 +1,7 @@
+package com.zifang.util.swing.workflow;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ util-workflow/src/main/java/com/zifang/util/workflow/engine/spark/services/algrithm/churn/LogisticTargetVaribleHandler.java
@@ -0,0 +1,4 @@
+package com.zifang.util.workflow.engine.spark.services.algrithm.churn;
+
+public class LogisticTargetVaribleHandler {
+}
