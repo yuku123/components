71077b8986380faceea187cb9d27e340c5405ccb
张陈浩 <chenhao.zhang@2piday.com>
2019-12-23 21:23:52 +0800

    add code

--- util-workflow/src/main/java/com/zifang/util/workflow/engine/spark/services/interceptor/CacheInterceptor.java
+++ util-workflow/src/main/java/com/zifang/util/workflow/engine/spark/services/interceptor/CacheInterceptor.java
@@ -1,7 +1,25 @@
 package com.zifang.util.workflow.engine.spark.services.interceptor;
 
+import com.zifang.util.core.aop.aspects.Aspect;
 import com.zifang.util.workflow.anoation.EngineServiceInterceptor;
 
+import java.lang.reflect.Method;
+
 @EngineServiceInterceptor
-public class CacheInterceptor {
+public class CacheInterceptor implements Aspect {
+    @Override
+    public boolean before(Object target, Method method, Object[] args) {
+        return false;
+    }
+
+    @Override
+    public boolean after(Object target, Method method, Object[] args, Object returnVal) {
+
+        return false;
+    }
+
+    @Override
+    public boolean afterException(Object target, Method method, Object[] args, Throwable e) {
+        return false;
+    }
 }
