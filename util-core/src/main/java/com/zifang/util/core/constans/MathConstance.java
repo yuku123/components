cc973c4fc7a484114bc0b6cd9c51f1cb2c7ba8e7
张陈浩 <chenhao.zhang@2piday.com>
2020-02-20 21:31:00 +0800

    ss

---
+++ util-core/src/main/java/com/zifang/util/core/constans/MathConstance.java
@@ -0,0 +1,4 @@
+package com.zifang.util.core.constans;
+
+public class MathConstance {
+}
---
+++ util-core/src/main/java/com/zifang/util/core/constans/TimeConstance.java
@@ -0,0 +1,8 @@
+package com.zifang.util.core.constans;
+
+public class TimeConstance {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(100000000);
+    }
+}
