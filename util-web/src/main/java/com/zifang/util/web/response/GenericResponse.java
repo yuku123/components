d21f5cf3fa1195d6986adf0d8b3f95361560ff26
张陈浩 <chenhao.zhang@2piday.com>
2020-04-01 11:00:55 +0800

    add code

---
+++ util-web/src/main/java/com/zifang/util/web/response/GenericResponse.java
@@ -0,0 +1,14 @@
+package com.zifang.util.web.response;
+
+import lombok.*;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class GenericResponse<T> extends BaseResponse{
+    private T data;
+}
---
+++ util-web/src/main/java/com/zifang/util/web/response/PaginationResponse.java
@@ -0,0 +1,17 @@
+package com.zifang.util.web.response;
+
+import lombok.*;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class PaginationResponse<T> extends BaseResponse {
+    private T data;//分页结果
+    private int limit;//每页条数
+    private int current;//当前页
+    private int total;//总数
+}
