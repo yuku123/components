acf2d7b76227271e7b79025a43112b4b8442294c
张陈浩 <chenhao.zhang@2piday.com>
2020-05-10 14:10:17 +0800

    add code

--- util-swing/src/main/java/com/zifang/util/swing/algrithm/lesson2/a/Client.java
+++ util-swing/src/main/java/com/zifang/util/swing/algrithm/lesson2/a/Client.java
@@ -4,7 +4,7 @@ public class Client {
     public static void main(String[] args) {
         int sceneWidth = 800;
         int sceneHeight = 800;
-        int N = 10;
+        int N = 100;
 
         AlgoVisualizer visualizer = new AlgoVisualizer(sceneWidth, sceneHeight, N);
     }
