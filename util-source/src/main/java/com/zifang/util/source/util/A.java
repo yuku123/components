d1a60e12e2b295e1a00ba8b5cbdf021203fafefd
张陈浩 <chenhao.zhang@2piday.com>
2020-08-09 23:18:50 +0800

    add code

---
+++ util-source/src/main/java/com/zifang/util/source/util/A.java
@@ -0,0 +1,4 @@
+package com.zifang.util.source.util;
+
+public class A {
+}
---
+++ util-xml/src/main/java/com/zifang/util/xml/XmlBeanUtil.java
@@ -0,0 +1,43 @@
+package com.zifang.util.xml;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+@Slf4j
+public class XmlBeanUtil {
+
+    public static String beanToXml(Object obj, Class<?> load) {
+        String xmlStr = null;
+        try {
+            JAXBContext context = JAXBContext.newInstance(load);
+            Marshaller marshaller = context.createMarshaller();
+            // 去掉生成xml的默认报文头
+            marshaller.setProperty(Marshaller.JAXB_FRAGMENT, true);
+            StringWriter writer = new StringWriter();
+            marshaller.marshal(obj, writer);
+            xmlStr = writer.toString();
+        } catch (JAXBException e) {
+            log.error("实体转为xml时出错!", e);
+        }
+        return xmlStr;
+    }
+
+    public static Object xmlToBean(String str, Class<?> load) {
+        Object object = null;
+        try {
+            JAXBContext context = JAXBContext.newInstance(load);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            object = unmarshaller.unmarshal(new StringReader(str));
+        } catch (JAXBException e) {
+            log.error("xml转换到实体时出错!", e);
+        }
+        return object;
+
+    }
+}
