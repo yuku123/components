217e3b7ba791fbd7c423ac962b1119b73ccd0167
张陈浩 <chenhao.zhang@2piday.com>
2020-09-03 09:49:57 +0800

    add code

--- util-core/src/main/java/com/zifang/util/core/beans/converter/Converter.java
+++ util-core/src/main/java/com/zifang/util/core/beans/converter/Converter.java
@@ -1,24 +1,5 @@
 package com.zifang.util.core.beans.converter;
 
-public class Converter<A,B> {
-
-    private Class<A> clazz;
-    private Class<B> target;
-
-    public Converter(Class<A> clazz,Class<B> target){
-        this.clazz = clazz;
-        this.target = target;
-    }
-
-    public B doConvert(Object a,Object b){
-        Byte.parseByte("");
-        Short.parseShort("");
-        return null;
-    }
-
-    public static void main(String[] args) {
-        for(int i = 0;i<100;i++){
-            System.out.println(((char)i)+":"+i);
-        }
-    }
+public interface Converter<F,T> {
+    T to(F f,T t);
 }
--- util-core/src/main/java/com/zifang/util/core/beans/converter/DefaultConverterRegister.java
+++ util-core/src/main/java/com/zifang/util/core/beans/converter/DefaultConverterRegister.java
@@ -1,4 +1,16 @@
 package com.zifang.util.core.beans.converter;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * 转换器的注册
+ * */
 public class DefaultConverterRegister {
+
+    private static Map<String,Object> stringObjectMap = new LinkedHashMap<>();
+
+    public static <F,T> void register(Converter<F,T> converter){
+
+    }
 }
