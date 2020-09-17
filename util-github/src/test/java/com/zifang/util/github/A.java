571afbe11bb776393cbd8ae41662e9087f0fde63
张陈浩 <chenhao.zhang@2piday.com>
2020-09-17 18:43:26 +0800

    add code

---
+++ util-github/src/test/java/com/zifang/util/github/A.java
@@ -0,0 +1,4 @@
+package com.zifang.util.github;
+
+public class A {
+}
---
+++ util-github/src/test/java/com/zifang/util/github/LoginTest.java
@@ -0,0 +1,28 @@
+package com.zifang.util.github;
+
+import org.junit.Test;
+import org.kohsuke.github.*;
+
+import java.io.IOException;
+import java.util.List;
+
+public class LoginTest {
+
+    private static String secret = "xx";
+
+    @Test
+    public  void test0() throws IOException {
+        GitHub github = new GitHubBuilder().withOAuthToken(secret).build();
+        GHCreateRepositoryBuilder g = github.createRepository("aaaaa");
+        g.private_(true);
+        g.create();
+    }
+
+    @Test
+    public void test1() throws IOException {
+        GitHub github = new GitHubBuilder().withOAuthToken(secret).build();
+        PagedIterable<GHRepository>s =  github.listAllPublicRepositories();
+        List<GHRepository> ss = s.toList();
+        System.out.println(ss);
+    }
+}
