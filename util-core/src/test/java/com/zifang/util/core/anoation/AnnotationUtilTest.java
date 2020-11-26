9dc031c17568378e2eb31471f7260628fb938115
张陈浩 <chenhao.zhang@2piday.com>
2020-11-26 17:28:51 +0800

    add code

--- util-core/src/test/java/com/zifang/util/core/anoation/AnnotationUtilTest.java
+++ util-core/src/test/java/com/zifang/util/core/anoation/AnnotationUtilTest.java
@@ -4,41 +4,107 @@ import com.zifang.util.core.annoations.AnnotationUtil;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 public class AnnotationUtilTest {
 	
 	@Test
-	public void t1() {
-		Object value = AnnotationUtil.getAnnotationValue(ClassWithAnnotation.class, AnnotationForTest.class);
-		Object v = AnnotationUtil.getAnnotationValue(ClassWithAnnotation.class, ClassInfo.class,"className");
-		System.out.println(v);
-		Object v1 = AnnotationUtil.getAnnotationValue(ClassWithAnnotation.class, superInfo.class,"superInfo");
-		Assert.assertEquals("测试", value);
-		Assert.assertEquals("superInfo", v1);
+	public void test1() {
+		Object value = AnnotationUtil.getAnnotationValue(WholeBase.class, ClassInfo.class);
+		Object className = AnnotationUtil.getAnnotationValue(WholeBase.class, ClassInfo.class,"className");
+		Object superInfo = AnnotationUtil.getAnnotationValue(WholeBase.class, SuperInfo.class,"superInfo");
+		Assert.assertEquals("default-class-value", value);
+		Assert.assertEquals("superInfo", superInfo);
+		Assert.assertEquals("test",className);
 	}
 }
 
-@ClassInfo(className = "name")
-@AnnotationForTest("测试")
-class ClassWithAnnotation{
+@ClassInfo(className = "test")
+class WholeBase {
 
-	private String name;
-	private String password;
+	@FieldInfo(name="a-private",password = "a-private")
+	private String a;
+	@FieldInfo(name="b-private",password = "b-private")
+	public String b;
+
+	@ConstructInfo(constructName = "wholeBase constructName")
+	public WholeBase(@ParameterInfo(setString = "test b")String b){}
+
+	@ConstructInfo(constructName = "wholeBase private constructName---parameter a")
+	private WholeBase(String a,@ParameterInfo(setString = "test b") String b){}
+
+	@Override
+	@Deprecated
+	@SuppressWarnings({"unchecked", "deprecation"})
+	@MethodInfo(author = "Pankaj", comments = "Main method", date = "Nov 17 2012", revision = 1)
+	public String toString() {
+		return "Overriden toString method";
+	}
+
+	@Deprecated
+	@SuppressWarnings({"unchecked", "deprecation"})
+	@MethodInfo(author = "sddsd", comments = "aa private", date = "sdsadasdsadsadsa", revision = 1)
+	private String aa() {
+		return "aa private";
+	}
+
+}
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+@Documented
+@Inherited
+@SuperInfo(superInfo = "superInfo")
+@interface ClassInfo {
+	String className();
+	String value() default "default-class-value";
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.ANNOTATION_TYPE})
+@interface SuperInfo {
+	String superInfo();
+	String value() default "default-class-value";
 
 }
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ ElementType.METHOD, ElementType.TYPE })
-@interface AnnotationForTest {
+@Target({ElementType.CONSTRUCTOR})
+@Documented
+@Inherited
+@interface ConstructInfo {
+	String constructName();
+}
 
-	/**
-	 * 注解的默认属性值
-	 *
-	 * @return 属性值
-	 */
-	String value();
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.PARAMETER})
+@Documented
+@Inherited
+@interface ParameterInfo {
+	String setString();
+}
+
+@Documented
+@Target(ElementType.METHOD)
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@interface MethodInfo {
+	String author() default "Pankaj";
+
+	String date();
+
+	int revision() default 1;
+
+	String comments();
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD})
+@Documented
+@Inherited
+@interface FieldInfo {
+	String name();
+	String password();
+	String comments() default "this is default parts";
 }
