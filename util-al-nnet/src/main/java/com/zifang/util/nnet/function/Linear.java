afaa8093ed01085e48d7805814fdb2d6a675176a
子房 <chanhao.zhang@come-future.com>
2021-06-26 23:17:42 +0800

    add code

--- util-al-nnet/src/main/java/com/zifang/util/nnet/function/Linear.java
+++ util-al-nnet/src/main/java/com/zifang/util/nnet/function/Linear.java
@@ -3,5 +3,9 @@ package com.zifang.util.nnet.function;
 /**
  * 线性激活函数
  */
-public class Linear {
+public class Linear implements IActivationFunction{
+    @Override
+    public Double calculate(Double x) {
+        return null;
+    }
 }
