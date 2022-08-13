6ca21e0acabbd3bd3c354ef06954863601fcf985
张陈浩 <chenhao.zhang@2piday.com>
2022-08-13 20:32:58 +0800

    add code

--- util-zex/src/main/java/com/zifang/util/zex/C.java
+++ util-zex/src/main/java/com/zifang/util/zex/C.java
@@ -1,5 +1,7 @@
 package com.zifang.util.zex;
 
+import java.time.LocalDateTime;
+
 public class C {
     int a = 5;
     char c = 'a';
@@ -7,8 +9,13 @@ public class C {
     float f = 5.12f;
     double d = 4.12;
 
+//    public static void main(String[] args) {
+//        int s = 0b0000_0000_0111_1101 & (1 << 4);
+//        System.out.println(s);
+//    }
+
     public static void main(String[] args) {
-        int s = 0b0000_0000_0111_1101 & (1 << 4);
-        System.out.println(s);
+        System.out.println(LocalDateTime.of(1996,8,1,0,0,0).plusDays(10000));
     }
+
 }
