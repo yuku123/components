90b2223b07b849c5b2fa16b3a7a8a695f7b369d7
张陈浩 <chenhao.zhang@2piday.com>
2023-07-25 20:31:28 +0800

    add code

---
+++ util-zex/src/main/java/com/zifang/util/zex/bust/ex/A.java
@@ -0,0 +1,11 @@
+package com.zifang.util.zex.bust.ex;
+
+public class A {
+
+    public static void main(String[] args) {
+        Runnable r = () ->{
+            System.out.println("");
+        };
+        r.run();
+    }
+}
