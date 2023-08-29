c658297bba2c60199921c43d259e0a0daa456063
子房 <chanhao.zhang@come-future.com>
2023-08-29 18:21:03 +0800

    add code

--- util-core/src/main/java/com/zifang/util/core/common/constant/StringConstant.java
+++ util-core/src/main/java/com/zifang/util/core/common/constant/StringConstant.java
@@ -6,15 +6,10 @@ public interface StringConstant {
     String ONE = "1";
     String TRUE = "true";
     String FALSE = "false";
-
     String EMPTY = "";
     String[] EMPTY_ARRAY = new String[0];
-
     String DEFAULT = "default";
-
     String FORMAT_SPECIFIER = "%(\\d+\\$)?([-#+ 0,(<]*)?(\\d+)?(\\.\\d+)?([tT])?([a-zA-Z%])";
-
     String FLOATING_POINT_NUMBER_FORMAT = "^[-\\+]?[.\\d]*$";
     String LINE_BREAK = "\n";
-
 }
