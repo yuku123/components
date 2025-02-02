d90034d398b44ac3b043a31e367197b9cf973b59
yuku123 <1340947819@qq.com>
2025-02-02 19:14:11 +0800

    d

---
+++ util-zex/src/main/java/com/zifang/util/zex/bust/chapter11/PathsTest.java
@@ -0,0 +1,12 @@
+package com.zifang.util.zex.bust.chapter11;
+
+import org.junit.Test;
+
+import java.nio.file.Paths;
+
+public class PathsTest {
+    @Test
+    public void test001(){
+        Paths.get("");
+    }
+}
