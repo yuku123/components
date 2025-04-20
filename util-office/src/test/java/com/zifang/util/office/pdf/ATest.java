5edc161de8a6ab2270b6ce55e9f8bc68d6ca5b4e
张陈浩 <chenhao.zhang@2piday.com>
2025-04-20 11:54:02 +0800

    add code

---
+++ util-office/src/test/java/com/zifang/util/office/pdf/ATest.java
@@ -0,0 +1,47 @@
+package com.zifang.util.office.pdf;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ATest {
+    @Test
+    public void test(){
+        String baseFolder ="/Volumes/Elements SE/out/test";
+        String targetFolder = "/Volumes/Elements SE/out/test_output";
+
+        List<String> existedFiles = new ArrayList<>();
+        for(File file : new File(targetFolder).listFiles()){
+            existedFiles.add(file.getName().replace(".epub.epub.pdf",""));
+        }
+
+        for(File file : new File(baseFolder).listFiles()){
+            String fileName = file.getName().replace(".epub","");
+            if(existedFiles.contains(fileName)){
+                //System.out.println(file.getName());
+                file.delete();
+            }
+        }
+    }
+
+    @Test
+    public void test2(){
+        String baseFolder ="/Volumes/Elements SE/out/mock2/test";
+        String targetFolder = "/Volumes/Elements SE/out/mock2/test_output";
+
+        List<String> existedFiles = new ArrayList<>();
+        for(File file : new File(targetFolder).listFiles()){
+            existedFiles.add(file.getName().replace(".azw3.azw3.pdf",""));
+        }
+
+        for(File file : new File(baseFolder).listFiles()){
+            String fileName = file.getName().replace(".azw3","");
+            if(existedFiles.contains(fileName)){
+                //System.out.println(file.getName());
+                file.delete();
+            }
+        }
+    }
+}
